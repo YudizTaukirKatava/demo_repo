b08e10ffb5ca5343388d4d5b703183503f644a86
Yudiz_TaukirKatava <130762213+YudizTaukirKatava@users.noreply.github.com>
2023-07-12 10:42:39 +0530

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# Define the remote server details
-REMOTE_HOST="3.89.205.58"
-REMOTE_USER="ubuntu"
-REMOTE_DIR="/home/ubuntu"
-
 # Get the list of deleted files using Git
-DELETED_FILES=$(git diff --name-only HEAD HEAD^ --diff-filter=D)
+DELETED_FILES=$(git diff --name-only --diff-filter=D HEAD^)
 
-# Connect to the remote server and delete the files
-ssh ${REMOTE_USER}@${REMOTE_HOST} "cd ${REMOTE_DIR} && rm ${DELETED_FILES}"
+# Delete the files on the production server
+rm -f ${DELETED_FILES}
 
-echo "Deleted files on the remote server: ${DELETED_FILES}"
+echo "Deleted files on the production server: ${DELETED_FILES}"
