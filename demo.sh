944871f59178fc60404fecd33c6b094973361d27
Yudiz_TaukirKatava <130762213+YudizTaukirKatava@users.noreply.github.com>
2023-07-12 14:06:23 +0530

    Delete demo.sh

--- demo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Get the list of deleted files using Git
-DELETED_FILES=$(git diff --name-only --diff-filter=D HEAD^)
-
-# Delete the files on the production server
-rm -f ${DELETED_FILES}
-
-echo "Deleted files on the production server: ${DELETED_FILES}"
