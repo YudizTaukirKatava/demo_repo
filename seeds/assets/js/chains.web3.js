f260eaaeeb8338e37ab4511354e9914a21b0362a
Ubuntu <ubuntu@ip-10-0-0-199.ap-south-1.compute.internal>
2023-07-21 06:18:11 +0000

    Added Production of nft-frontend

---
+++ seeds/assets/js/chains.web3.js
@@ -0,0 +1,50 @@
+const _chains_web3 = {
+    56: {
+        chainId: '0x38', // 56
+        chainName: 'Binance Smart Chain',
+        // network: 'BSC',
+        nativeCurrency: {
+            name: 'BNB',
+            symbol: 'BNB',
+            decimals: 18,
+        },
+        rpcUrls: ['https://rpc.ankr.com/bsc'],
+        blockExplorerUrls: ['https://bscscan.com'],
+    },
+    1: {
+        chainId: '0x1', // 1
+        chainName: 'Ethereum Mainnet',
+        // network: 'Ethereum',
+        nativeCurrency: {
+            name: 'Ether',
+            symbol: 'ETH',
+            decimals: 18,
+        },
+        rpcUrls: ['https://rpc.ankr.com/eth'],
+        blockExplorerUrls: ['https://etherscan.io'],
+    },
+    43114: {
+        chainId: '0xa86a', // 43114
+        chainName: 'Avalanche c chain',
+        // network: 'Avalanche',
+        nativeCurrency: {
+            name: 'Avalanche',
+            symbol: 'AVAX',
+            decimals: 18,
+        },
+        rpcUrls: ['https://rpc.ankr.com/avalanche'],
+        blockExplorerUrls: ['https://snowtrace.io'],
+    },
+    137: {
+        chainId: '0x89', // 137
+        chainName: 'Polygon',
+        // network: 'Polygon',
+        nativeCurrency: {
+            name: 'Polygon',
+            symbol: 'MATIC',
+            decimals: 18,
+        },
+        rpcUrls: ['https://rpc.ankr.com/polygon'],
+        blockExplorerUrls: ['https://polygonscan.com'],
+    },
+};
